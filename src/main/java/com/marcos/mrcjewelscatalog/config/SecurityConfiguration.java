39d053e1cfcaf4eeebccba4b15fc480052f98c4d
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-02-01 00:44:31 -0300

    afsiiguyasifgasfig

--- src/main/java/com/marcos/mrcjewelscatalog/config/SecurityConfiguration.java
+++ src/main/java/com/marcos/mrcjewelscatalog/config/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -62,7 +63,7 @@ public class SecurityConfiguration {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-    configuration.setAllowedOrigins(Arrays.asList("https://mrc-jewels-catalog-backend-production.up.railway.app/"));
+    configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
